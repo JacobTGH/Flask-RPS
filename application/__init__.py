d957202d010bf52d0e956de8e77c63ef455c58bd
JacobTGH <139144703+JacobTGH@users.noreply.github.com>
2023-08-08 12:00:17 +0100

    Commented out code in __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,7 @@ import secrets
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
-app.config["SECRET_KEY"] = 
+# app.config["SECRET_KEY"] = 
 
 db = SQLAlchemy(app)
 
